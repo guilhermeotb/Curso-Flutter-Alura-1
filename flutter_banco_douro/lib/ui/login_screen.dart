fa7ef5a1e6bbbbef7c1b8dbf0f7c50c4ff7a2555
guilhermeotb <guiguipereira777@gmail.com>
2026-03-23 16:49:41 -0300

    #2 Estilização da tela de login (textos e botão)

--- flutter_banco_douro/lib/ui/login_screen.dart
+++ flutter_banco_douro/lib/ui/login_screen.dart
@@ -6,15 +6,35 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Column(children: <Widget>[
-         const Text("Sistema de Gestão de Contas"),
-         TextFormField(),
-         TextFormField(),
-         ElevatedButton(
-          onPressed: (){},
-          child: const Text("Entrar"),
-         ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+           const Text(
+            "Sistema de Gestão de Contas",
+            textAlign: TextAlign.center,
+             style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+             ),
+           TextFormField(decoration: const InputDecoration(label: Text("E-mail")),),
+           TextFormField(obscureText: true, decoration: const InputDecoration(label: Text("Senha")), ),
+           SizedBox(height: 32,),
+           ElevatedButton(
+            onPressed: (){},
+            style: ButtonStyle( 
+              backgroundColor: WidgetStateProperty.all(
+                Color(0xFFFFA902),
+                 ),
+            ),
+            child: const Text(
+              "Entrar",
+               style: TextStyle(color: Colors.black),
+
+               ),
+           ),
+          ],
+        ),
       ),
     );
   }

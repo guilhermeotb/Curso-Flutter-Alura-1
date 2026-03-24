e5f265d5e5cf3a89cd8470f8837721910ea2ed90
guilhermeotb <guiguipereira777@gmail.com>
2026-03-24 13:00:48 -0300

    #2 Desafio  de centralizar a UI do flutter_review

--- flutter_reviews/lib/ui/review_screen.dart
+++ flutter_reviews/lib/ui/review_screen.dart
@@ -8,27 +8,28 @@ class ReviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("${review.title} (${review.year})",
-          style: const TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${review.title} (${review.year})",
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: List.generate(review.stars, (index) {
+                  return const Text("⭐", style: TextStyle(fontSize: 20));
+                }),
+              ),
+          
+              Text("Data: ${review.date.toString().substring(0, 16)}"),
+              Text(review.review, style: TextStyle(fontSize: 14)),
+            ],
           ),
-          Row(
-            children: List.generate( review.stars,
-            (index){
-              return const Text("⭐", style: TextStyle(fontSize: 20),);
-            },
-          ),
-        ),
-        
-        Text( "Data: ${review.date.toString().substring(0, 10)}",),
-        Text(review.review),
-        
-      ],
-    )
-  );
-}
+      ),
+    );
+  }
 }
